da4821324f3fe042564362b2634d1375d1ff24b9
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-20 18:41:42 +0100

    feat: implement random module

---
+++ src/random.c
@@ -0,0 +1,37 @@
+#include "random.h"
+
+void init_random() {
+    static bool initialized = false;
+    if (!initialized) {
+        initialized = true;
+        time_t t;
+        srand((unsigned)time(&t));
+    }
+}
+
+size_t random_from_range(size_t low_bound_inclusive, size_t top_bound_inclusive) {
+    assert(low_bound_inclusive < top_bound_inclusive);
+    init_random();
+    return low_bound_inclusive + (rand() % (top_bound_inclusive - low_bound_inclusive + 1));
+}
+
+void random_weighted_gf4_poly(gf4_poly_t * poly, size_t weight) {
+    assert(NULL != poly);
+    assert(weight <= poly->capacity);
+    for (size_t i = 0; i < weight; ++i) {
+        poly->coefficients[i] = (gf4_t) random_from_range(1, GF4_MAX_VALUE);
+    }
+    for (size_t i = 0; i < poly->capacity; ++i) {
+        size_t j = (size_t)random_from_range(i, poly->capacity - 1);
+        gf4_t tmp = poly->coefficients[i];
+        poly->coefficients[i] = poly->coefficients[j];
+        poly->coefficients[j] = tmp;
+    }
+    poly->degree = 0;
+    for (size_t i = poly->capacity - 1; i != 0; --i) {
+        if (0 != poly->coefficients[i]) {
+            poly->degree = i;
+            break;
+        }
+    }
+}
---
+++ src/random.h
@@ -0,0 +1,41 @@
+#ifndef MDPC_GF4_RANDOM_H
+#define MDPC_GF4_RANDOM_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include "gf4_poly.h"
+#include "gf4.h"
+
+
+/**
+ * Initialize random using srand with current time.
+ *
+ * Ensures that srand is called at most once during each program execution. This function is not thread safe.
+ * This function doesn't need to be called explicitly.
+ */
+void init_random();
+
+/**
+ * Return an unsigned integer within given range.
+ *
+ * May also call init_random().
+ * This function uses rand() to generate random values.
+ *
+ * @param low_bound_inclusive inclusive low bound
+ * @param top_bound_inclusive inclusive top bound
+ * @return an unsigned integer i s. t. low_bound_inclusive <= i <= top_bound_inclusive
+ */
+size_t random_from_range(size_t low_bound_inclusive, size_t top_bound_inclusive);
+
+/**
+ * Generate a polynomial of given weight.
+ *
+ * May also call init_random().
+ *
+ * @param poly output polynomial, it must be allocated in advance
+ * @param weight number of nonzero coefficients
+ */
+void random_weighted_gf4_poly(gf4_poly_t * poly, size_t weight);
+
+#endif //MDPC_GF4_RANDOM_H
