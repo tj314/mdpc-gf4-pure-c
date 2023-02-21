cdffacc2ff76e1d7db0fdf0bbcdd5e2b0e876449
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-21 20:40:48 +0100

    chore: add files for encoder

---
+++ src/encoder.c
@@ -0,0 +1,5 @@
+//
+// Created by tj on 21.2.2023.
+//
+
+#include "encoder.h"
---
+++ src/encoder.h
@@ -0,0 +1,8 @@
+//
+// Created by tj on 21.2.2023.
+//
+
+#ifndef MDPC_GF4_ENCODER_H
+#define MDPC_GF4_ENCODER_H
+
+#endif //MDPC_GF4_ENCODER_H
