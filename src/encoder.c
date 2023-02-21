e16d6cc3455813e522402d792e89c653d670ea84
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-21 21:01:14 +0100

    feat: implement encoding

--- src/encoder.c
+++ src/encoder.c
@@ -1,5 +1,18 @@
-//
-// Created by tj on 21.2.2023.
-//
-
 #include "encoder.h"
+
+
+void encode(gf4_poly_t * out_encoded, gf4_poly_t * in_message, encoding_context_t * ctx) {
+    assert(NULL != out_encoded);
+    assert(NULL != in_message);
+    assert(NULL != ctx);
+    assert(out_encoded->capacity >= 2*ctx->block_size);
+    assert(in_message >= ctx->block_size);
+    memcpy(out_encoded->coefficients, in_message->coefficients, ctx->block_size);
+    for (size_t i = 0; i < ctx->block_size; ++i) {
+        gf4_t tmp = 0;
+        for (size_t j = 0; j < ctx->block_size; ++j) {
+            tmp ^= gf4_mul(in_message->coefficients[j], ctx->second_block_G.coefficients[(i + j) % ctx->block_size]);
+        }
+        out_encoded->coefficients[ctx->block_size + i] = tmp;
+    }
+}
--- src/encoder.h
+++ src/encoder.h
@@ -1,8 +1,19 @@
-//
-// Created by tj on 21.2.2023.
-//
-
 #ifndef MDPC_GF4_ENCODER_H
 #define MDPC_GF4_ENCODER_H
+
+#include "gf4.h"
+#include "gf4_poly.h"
+#include "contexts.h"
+
+/**
+ * Encode a message.
+ *
+ * out_encoded must be initialized in advance with sufficient capacity. It must be a zero polynomial.
+ *
+ * @param out_encoded polynomial to store the result in
+ * @param in_message polynomial containing a message to encode
+ * @param ctx a valid encoding context
+ */
+void encode(gf4_poly_t * out_encoded, gf4_poly_t * in_message, encoding_context_t * ctx);
 
 #endif //MDPC_GF4_ENCODER_H
