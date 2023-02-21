3a431e383a0c810b8b3302d78ad7438744731673
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-21 16:57:34 +0100

    docs: add documentation to contexts_generate function

--- src/contexts.h
+++ src/contexts.h
@@ -17,6 +17,20 @@ typedef struct {
     size_t block_size;
 } decoding_context_t;
 
-
+/**
+ * Generate contexts for encoding and decoding.
+ *
+ * generate polynomials h0 and h1 st. hamming weight of h0 (and also h1) is equal to block_weight.
+ * generated polynomial h1 will be invertible mod (x^block_size + 1). h0 and h1 are used for decoding.
+ * polynomial for encoding is calculated as follows: (h0 * h1^-1) mod (x^block_size + 1).
+ *
+ * This function will allocate memory for the polynomials. Do not initialize polynomials in out_enc_ctx and
+ * out_dec_ctx yourself!
+ *
+ * @param out_enc_ctx memory location to store encoding polynomials and parameters to
+ * @param out_dec_ctx memory location to store decoding polynomials and parameters to
+ * @param block_size size of the circulant block of the matrices H, G
+ * @param block_weight hamming weight of each row/columns of the circulant blocks of H and G
+ */
 void contexts_genarate(encoding_context_t * out_enc_ctx, decoding_context_t * out_dec_ctx, size_t block_size, size_t block_weight);
 #endif //MDPC_GF4_CONTEXTS_H
