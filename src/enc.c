6e1def53f3a52228d5d7e7b965b5008617e8c57f
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-22 07:27:00 +0100

    fix: fix incorrect encoding, rows of matrix G were shifted in incorrect order

--- src/enc.c
+++ src/enc.c
@@ -8,11 +8,11 @@ void enc_encode(gf4_poly_t * out_encoded, gf4_poly_t * in_message, encoding_cont
     assert(out_encoded->capacity >= 2*ctx->block_size);
     assert(in_message->capacity >= ctx->block_size);
     memcpy(out_encoded->coefficients, in_message->coefficients, ctx->block_size);
-    for (size_t i = 0; i < ctx->block_size; ++i) {
+    for (size_t i = ctx->block_size; i > 0; --i) {
         gf4_t tmp = 0;
         for (size_t j = 0; j < ctx->block_size; ++j) {
             tmp ^= gf4_mul(in_message->coefficients[j], ctx->second_block_G.coefficients[(i + j) % ctx->block_size]);
         }
-        out_encoded->coefficients[ctx->block_size + i] = tmp;
+        out_encoded->coefficients[2*ctx->block_size - i] = tmp;
     }
 }
