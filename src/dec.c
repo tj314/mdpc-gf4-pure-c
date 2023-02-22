eaf7bb861836a6ad55ce48c18678802cf97018e7
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-22 07:27:34 +0100

    feat: implement syndrome calculation

--- src/dec.c
+++ src/dec.c
@@ -1,5 +1,24 @@
-//
-// Created by tj on 21.2.2023.
-//
-
 #include "dec.h"
+
+void dec_calculate_syndrome(gf4_poly_t * out_syndrome, gf4_poly_t * in_vector, decoding_context_t * ctx) {
+    assert(NULL != out_syndrome);
+    assert(NULL != in_vector);
+    assert(NULL != ctx);
+    assert(out_syndrome->capacity >= ctx->block_size);
+    assert(in_vector->capacity >= 2*ctx->block_size);
+    for (size_t i = ctx->block_size; i > 0; --i) {
+        gf4_t tmp = 0;
+        for (size_t j = 0; j < ctx->block_size; ++j) {
+            tmp ^= gf4_mul(ctx->h0.coefficients[(i+j) % ctx->block_size], in_vector->coefficients[j]);
+            tmp ^= gf4_mul(ctx->h1.coefficients[(i+j) % ctx->block_size], in_vector->coefficients[ctx->block_size + j]);
+        }
+        out_syndrome->coefficients[ctx->block_size - i] = tmp;
+    }
+    out_syndrome->degree = 0;
+    for (size_t i = out_syndrome->capacity; i > 0; --i) {
+        if (0 != out_syndrome->coefficients[i]) {
+            out_syndrome->degree = i;
+            break;
+        }
+    }
+}
--- src/dec.h
+++ src/dec.h
@@ -1,8 +1,19 @@
-//
-// Created by tj on 21.2.2023.
-//
-
 #ifndef MDPC_GF4_DEC_H
 #define MDPC_GF4_DEC_H
+
+#include "gf4.h"
+#include "gf4_poly.h"
+#include "contexts.h"
+
+/**
+ * Calculate the syndrome of a vector.
+ *
+ * out_syndrome must be initialized in advance. It must have capacity of at least block_size. It must be a zero polynomial.
+ *
+ * @param out_syndrome pointer to a polynomial that will be used to store the calculated syndrome
+ * @param in_vector pointer to a polynomial representing a vector
+ * @param ctx a valid decoding context
+ */
+void dec_calculate_syndrome(gf4_poly_t * out_syndrome, gf4_poly_t * in_vector, decoding_context_t * ctx);
 
 #endif //MDPC_GF4_DEC_H
