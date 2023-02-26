eaf232f8570fb8551475a08c20662c38233077db
tj314 <po-ca-hon-tas@tutanota.com>
2023-02-26 21:49:23 +0100

    fix: fix a bug where h1 polynomial could be generated with incorrect hamming weight

--- src/contexts.c
+++ src/contexts.c
@@ -24,11 +24,11 @@ void contexts_init(encoding_context_t * out_enc_ctx, decoding_context_t * out_de
     gf4_poly_t h1 = gf4_poly_init_zero(capacity);
     gf4_poly_t maybe_inverse = gf4_poly_init_zero(2*capacity);
     random_weighted_gf4_poly(&h0, block_size, block_weight);
+    random_weighted_gf4_poly(&h1, block_size, block_weight);
 
     int repetitions = 0;
 
     while (true) {
-        random_weighted_gf4_poly(&h1, block_size, block_weight);
         while (0 == poly_sum(&h1)) {
             gf4_poly_zero_out(&h1);
             random_weighted_gf4_poly(&h1, block_size, block_weight);
@@ -71,6 +71,7 @@ void contexts_init(encoding_context_t * out_enc_ctx, decoding_context_t * out_de
             return;
         }
         gf4_poly_zero_out(&maybe_inverse);
+        gf4_poly_zero_out(&h1);
         repetitions++;
         if (repetitions > 20) {
             random_force_reseed();
