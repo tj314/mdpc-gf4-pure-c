35037ffef7a64aad73530ed5d542b910fc2ae6bc
tj314 <po-ca-hon-tas@tutanota.com>
2023-04-11 13:50:45 +0200

    feat: implement a basic cmdline interface for main

--- main.c
+++ main.c
@@ -1,170 +1,11 @@
 #include <stdio.h>
+#include <pthread.h>
 #include "src/gf4.h"
 #include "src/gf4_poly.h"
 #include "src/contexts.h"
 #include "src/enc.h"
 #include "src/dec.h"
 
-
-void test_load() {
-    size_t block_size = 2339;
-    size_t block_weight = 37;
-    // size_t block_size = 7;
-    // size_t block_weight = 3;
-    encoding_context_t ec_gen, ec_load;
-    decoding_context_t dc_gen, dc_load;
-    fprintf(stderr, "Generating contexts!\n");
-    contexts_init(&ec_gen, &dc_gen, block_size, block_weight);
-    fprintf(stderr, "Contexts generated!\n");
-
-    contexts_save("save.bin", &ec_gen, &dc_gen);
-    contexts_load("save.bin", block_size, &ec_load, &dc_load);
-
-    for (size_t i = 0; i < block_size; ++i) {
-        if (ec_gen.second_block_G.coefficients[i] != ec_load.second_block_G.coefficients[i] ||
-        dc_gen.h0.coefficients[i] != dc_load.h0.coefficients[i] ||
-        dc_gen.h1.coefficients[i] != dc_load.h1.coefficients[i]) {
-            fprintf(stderr, "coefficients do not match!\n");
-            exit(-1);
-        }
-    }
-    fprintf(stderr, "all ok!\n");
-    contexts_deinit(&ec_gen, &dc_gen);
-    contexts_deinit(&ec_load, &dc_load);
-}
-
-void test_inverse() {
-    gf4_poly_t poly1 = gf4_poly_init_zero(10);
-    gf4_poly_t poly2 = gf4_poly_init_zero(10);
-    gf4_poly_t modulus = gf4_poly_init_zero(10);
-    gf4_poly_t maybe_inverse = gf4_poly_init_zero(10);
-
-    gf4_poly_set_coefficient(&poly1, 2, 1);
-    gf4_poly_set_coefficient(&poly1, 1, 1);
-    gf4_poly_set_coefficient(&poly1, 0, 1);
-
-    gf4_poly_set_coefficient(&poly2, 4, 2);
-    gf4_poly_set_coefficient(&poly2, 1, 2);
-
-    gf4_poly_set_coefficient(&modulus, 0, 1);
-    gf4_poly_set_coefficient(&modulus, 8, 1);
-
-    gf4_poly_t * poly = &poly1;
-    bool inverse_exists = gf4_poly_invert_slow(&maybe_inverse, poly, &modulus);
-
-
-    printf("poly:    ");
-    gf4_poly_pretty_print(poly, stderr, "\n");
-    printf("modulus: ");
-    gf4_poly_pretty_print(&modulus, stderr, "\n");
-
-    if (inverse_exists) {
-        printf("inverse: ");
-        gf4_poly_pretty_print(&maybe_inverse, stderr, "\n");
-        gf4_poly_t div = gf4_poly_init_zero(10);
-        gf4_poly_t rem = gf4_poly_init_zero(10);
-        gf4_poly_t m = gf4_poly_init_zero(20);
-        gf4_poly_mul(&m, &maybe_inverse, poly);
-        gf4_poly_div_rem(&div, &rem, &m, &modulus);
-        printf("inv*poly %% modulus: ");
-        gf4_poly_pretty_print(&rem, stderr, "\n");
-        gf4_poly_deinit(&div);
-        gf4_poly_deinit(&rem);
-        gf4_poly_deinit(&m);
-    } else {
-        printf("inverse does not exist!\n");
-    }
-
-    gf4_poly_deinit(&poly1);
-    gf4_poly_deinit(&poly2);
-    gf4_poly_deinit(&modulus);
-    gf4_poly_deinit(&maybe_inverse);
-}
-
-void test_encrypt_decrypt(size_t block_size, size_t block_weight, size_t num_errors, size_t num_iterations) {
-    encoding_context_t ec;
-    decoding_context_t dc;
-    contexts_init(&ec, &dc, block_size, block_weight);
-
-    gf4_poly_t msg = gf4_poly_init_zero(block_size);
-    gf4_poly_t encrypted = gf4_poly_init_zero(2*block_size);
-    gf4_poly_t decoded = gf4_poly_init_zero(2*block_size);
-
-    random_weighted_gf4_poly(&msg, block_size, block_weight);
-    enc_encrypt(&encrypted, &msg, num_errors, &ec);
-
-    bool decoding_success = dec_decrypt(&decoded, &encrypted, dec_decode_symbol_flipping, num_iterations, &dc);
-    if (decoding_success) {
-        fprintf(stderr, "decryption success!\n");
-        fprintf(stderr, "msg: ");
-        gf4_poly_coeff_print(&msg, block_size, stderr, "\n");
-        fprintf(stderr, "dec: ");
-        gf4_poly_coeff_print(&decoded, block_size, stderr, "\n");
-        bool matching = true;
-        size_t num_misses = 0;
-        for (size_t i = 0; i < block_size; ++i) {
-            bool tmp = decoded.coefficients[i] == msg.coefficients[i];
-            matching = matching && tmp;
-            num_misses += (size_t)tmp;
-        }
-        if (matching) {
-            fprintf(stderr, "decryption correct!\n");
-        } else {
-            fprintf(stderr, "decryption incorrect!!!\n");
-            fprintf(stderr, "num misses: %zu\n", num_misses);
-        }
-    } else {
-        fprintf(stderr, "decryption failure!\n");
-    }
-
-    gf4_poly_deinit(&msg);
-    gf4_poly_deinit(&encrypted);
-    gf4_poly_deinit(&decoded);
-    contexts_deinit(&ec, &dc);
-}
-
-void test_encrypt_decrypt_from_file(const char * filepath,
-                                    size_t block_size, size_t block_weight, size_t num_errors, size_t num_iterations) {
-    encoding_context_t ec;
-    decoding_context_t dc;
-    contexts_load(filepath, block_size, &ec, &dc);
-
-    gf4_poly_t msg = gf4_poly_init_zero(block_size);
-    gf4_poly_t encrypted = gf4_poly_init_zero(2*block_size);
-    gf4_poly_t decoded = gf4_poly_init_zero(2*block_size);
-
-    random_weighted_gf4_poly(&msg, block_size, block_weight);
-    enc_encrypt(&encrypted, &msg, num_errors, &ec);
-
-    bool decoding_success = dec_decrypt(&decoded, &encrypted, dec_decode_symbol_flipping, num_iterations, &dc);
-    if (decoding_success) {
-        fprintf(stderr, "decryption success!\n");
-        fprintf(stderr, "msg: ");
-        gf4_poly_coeff_print(&msg, block_size, stderr, "\n");
-        fprintf(stderr, "dec: ");
-        gf4_poly_coeff_print(&decoded, block_size, stderr, "\n");
-        bool matching = true;
-        size_t num_misses = 0;
-        for (size_t i = 0; i < block_size; ++i) {
-            bool tmp = decoded.coefficients[i] == msg.coefficients[i];
-            matching = matching && tmp;
-            num_misses += (size_t)tmp;
-        }
-        if (matching) {
-            fprintf(stderr, "decryption correct!\n");
-        } else {
-            fprintf(stderr, "decryption incorrect!!!\n");
-            fprintf(stderr, "num misses: %zu\n", num_misses);
-        }
-    } else {
-        fprintf(stderr, "decryption failure!\n");
-    }
-
-    gf4_poly_deinit(&msg);
-    gf4_poly_deinit(&encrypted);
-    gf4_poly_deinit(&decoded);
-    contexts_deinit(&ec, &dc);
-}
 
 void experiment0(size_t block_size, size_t num_errors, size_t M, const char * out_filename) {
     encoding_context_t ec;
@@ -241,6 +82,7 @@ void experiment1(size_t block_size, size_t num_errors, size_t M, const char * ou
     gf4_poly_deinit(&err_vector);
     contexts_deinit(&ec, &dc);
 }
+
 void gen_keys(size_t block_size, size_t block_weight) {
     encoding_context_t ec;
     decoding_context_t dc;
@@ -250,22 +92,117 @@ void gen_keys(size_t block_size, size_t block_weight) {
 }
 
 
-
-int main(void) {
-    // test_inverse();
-    // test_load();
-
+void run_tests(FILE * file, size_t num_keys, size_t num_messages, size_t block_size, size_t block_weight, size_t num_errors, size_t num_iterations, size_t decoder, size_t delta) {
+    /*
     size_t block_size = 2339;
     size_t block_weight = 37;
     size_t num_errors = 84;
     size_t num_iterations = 150;
-    // size_t block_size = 7;
-    // size_t block_weight = 3;
-    // size_t num_errors = 1;
-    // size_t num_iterations = 10;
-    // test_encrypt_decrypt(block_size, block_weight, num_errors, num_iterations);
+    */
+    bool (*decode_function)(gf4_poly_t*, gf4_poly_t *, size_t, decoding_context_t *);
+    switch (decoder) {
+        case 0:
+            decode_function = &dec_decode_symbol_flipping;
+            break;
+        case 1:
+            decode_function = &dec_decode_symbol_flipping_2;
+            break;
+        case 2:
+            decode_function = &dec_decode_symbol_flipping_delta;
+            break;
+        default:
+            decode_function = NULL;
+            exit(-1);
+    }
+    gf4_poly_t plaintext = gf4_poly_init_zero(block_size);
+    gf4_poly_t ciphertext = gf4_poly_init_zero(2 * block_size);
+    gf4_poly_t decrypted = gf4_poly_init_zero(2 * block_size);
+    size_t failure_counter = 0;
+    for (size_t i = 0; i < num_keys; ++i) {
+        fprintf(stderr, "regen keys!\n");
+        encoding_context_t ec;
+        decoding_context_t dc;
+        contexts_init(&ec, &dc, block_size, block_weight);
 
-    gen_keys(block_size, block_weight);
+        dc.delta_setting = (long)delta; // this setting does not matter for decoders that do not use delta // TODO: remove
 
+        for (size_t j = 0; j < num_messages; ++j) {
+            random_gf4_poly(&plaintext, block_size);
+            enc_encrypt(&ciphertext, &plaintext, num_errors, &ec);
+            bool decryption_success = dec_decrypt(&decrypted, &ciphertext, dec_decode_symbol_flipping, num_iterations,
+                                                  &dc);
+            if (decryption_success) {
+                fprintf(stderr, "progress: %zu / %zu,  SUCCESS\n", j + 1, num_messages*num_keys);
+            } else {
+                failure_counter += 1;
+                fprintf(stderr, "progress: %zu / %zu,  FAILURE\n", j + 1, num_messages*num_keys);
+            }
+            gf4_poly_zero_out(&plaintext);
+            gf4_poly_zero_out(&ciphertext);
+            gf4_poly_zero_out(&decrypted);
+        }
+        contexts_deinit(&ec, &dc);
+    }
+    gf4_poly_deinit(&plaintext);
+    gf4_poly_deinit(&ciphertext);
+    gf4_poly_deinit(&decrypted);
+    fprintf(file, "num failures: %zu / %zu\n", failure_counter, num_messages);
+}
+
+
+void print_usage() {
+    fprintf(stderr, "Usage: ./mdpc-gf4 NUM_KEYS NUM_MSGS BLOCK_SIZE BLOCK_WEIGHT NUM_ERRORS NUM_ITERS DECODER [DELTA]\n");
+    fprintf(stderr, "e.g.:  ./mdpc-gf4 10 100 2293 37 88 200 0\n");
+    fprintf(stderr, "\nParameter values:\n");
+    fprintf(stderr, "NUM_KEYS:     positive integer, number of key pairs to generate\n");
+    fprintf(stderr, "NUM_MSGS:     positive integer, number of messages to generate, encrypt and decrypt for every key pair\n");
+    fprintf(stderr, "BLOCK_SIZE:   positive integer, size of cyclic matrix block, 2293 or 2339 is recommended\n");
+    fprintf(stderr, "BLOCK_WEIGHT: positive integer, hamming weight of cyclic matrix block, 37 is recommended\n");
+    fprintf(stderr, "NUM_ERRORS:   positive integer, number of errors in the error vector\n");
+    fprintf(stderr, "NUM_ITERS:    positive integer, number of decoding iterations\n");
+    fprintf(stderr, "DECODER:      positive integer, one of the options listed bellow\n");
+    fprintf(stderr, "DELTA:        optional positive integer, used as delta with decoder 2\n");
+    fprintf(stderr, "\nPossible decoders:\n");
+    fprintf(stderr, "0 --> SF v1\n");
+    fprintf(stderr, "1 --> SF v2\n");
+    fprintf(stderr, "2 --> SF with delta\n");
+    fprintf(stderr, "3 --> SF with thr\n");
+}
+
+int main(int argc, char ** argv) {
+    /*
+    size_t block_size = 2293;
+    size_t block_weight = 37;
+    size_t num_iterations = 150;
+    size_t num_messages = 200;
+    */
+    if (8 != argc && 9 != argc) {
+        print_usage();
+        return 0;
+    }
+    size_t num_keys;
+    size_t num_messages;
+    size_t block_size;
+    size_t block_weight;
+    size_t num_errors;
+    size_t num_iterations;
+    size_t decoder;
+    size_t delta;
+    num_keys = atol(argv[1]);
+    num_messages = atol(argv[2]);
+    block_size = atol(argv[3]);
+    block_weight = atol(argv[4]);
+    num_errors = atol(argv[5]);
+    num_iterations = atol(argv[6]);
+    decoder = atol(argv[7]);
+    fprintf(stderr, "Params are: %zu %zu %zu %zu %zu %zu %zu", num_keys, num_messages, block_size, block_weight, num_errors, num_iterations, decoder);
+    if (8 == argc) {
+        fprintf(stderr, "\n");
+        run_tests(stderr, num_keys, num_messages, block_size, block_weight, num_errors, num_iterations, decoder, 0);
+    } else { // delta is provided
+        delta = atol(argv[8]);
+        fprintf(stderr, " %zu\n", delta);
+        run_tests(stderr, num_keys, num_messages, block_size, block_weight, num_errors, num_iterations, decoder, delta);
+    }
     return 0;
 }
