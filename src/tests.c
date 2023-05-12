26712ea3bf62a7181a23d2a6862b150e394a0de6
tj314 <po-ca-hon-tas@tutanota.com>
2023-05-12 15:44:58 +0200

    chore: add file headers to comply with GPL v3

--- src/tests.c
+++ src/tests.c
@@ -1,4 +1,21 @@
-//#ifdef RUNTESTS
+/*
+ This file is part of QC-MDPC McEliece over GF(4) implementation.
+ Copyright (C) 2023 Tomáš Vavro
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "tests.h"
 
 void print_OK() {
@@ -340,4 +357,3 @@ void test_dec_calculate_syndrome() {
     gf4_poly_deinit(&syndrome);
     print_OK();
 }
-//#endif
