3b6e92f2235a769d7e5448285d8fdec7506475ad
tj314 <po-ca-hon-tas@tutanota.com>
2023-08-12 15:52:33 +0200

    style: add file headers

--- src/gf4_vector.h
+++ src/gf4_vector.h
@@ -1,6 +1,27 @@
-//
-// Created by tj on 10.8.2023.
-//
+/**
+ *  @file   gf4_vector.h
+ *  @brief  Vectors over GF(4).
+ *  @author Tomáš Vavro
+ *  @date   2023-08-12
+ ***********************************************/
+
+/*
+ This file is part of QC-MDPC McEliece over GF(4) implementation.
+ Copyright (C) 2023 Tomáš Vavro
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef MDPC_GF4_GF4_VECTOR_H
 #define MDPC_GF4_GF4_VECTOR_H
